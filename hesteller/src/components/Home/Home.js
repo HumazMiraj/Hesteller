0ff2b3e7e6403c41013684736748d625d2e1e9f1
HumazMiraj <humaz.arhams@gmail.com>
2021-11-04 18:59:00 +0500

    first commit

---
+++ hesteller/src/components/Home/Home.js
@@ -0,0 +1,130 @@
+
+function Home() {
+  return (
+    <>
+    <section className="banner-section">
+        <div className="custom-container">
+            <div className="content-area text-center">
+                <div className="logo">
+                    <img src="images/logo.png" className="" alt="" width="150" />
+                </div>
+                <h1>Seamless & supportive incubation <br /><span className="text-pink">for technology founders</span></h1>
+                <h4>We work with global and local teams on software and hardware projects to bring your ideas to life
+                </h4>
+                <button className="btn btn-primary">Start Ideating</button>
+            </div>
+
+        </div>
+    </section>
+    <section className="banner-bottom-section text-center">
+        <div className="custom-container">
+            <h3 className="text-pink"><b>We partner with you for the long term</b></h3>
+        </div>
+    </section>
+    <section className="with-clients-section text-center">
+        <div className="custom-container">
+            <h2 className="text-pink heading">With clients from 62+ countries</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="item">
+                        <div className="image">
+                            <img src="images/icon-1.png" alt="" className="img-fluid" />
+                        </div>
+                        <p>We delve deep to understand the problem you are solving. We incubate this idea till the
+                            solution is apparent</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <div className="image">
+                            <img src="images/icon-2.png" alt="" className="img-fluid" />
+                        </div>
+                        <p>We work closely with you and leverage our ecosystem of partners to provide you with the
+                            technology needed to build your idea</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <div className="image">
+                            <img src="images/icon-3.png" alt="" className="img-fluid" />
+                        </div>
+                        <p>We help startups expand their built solution to their target audience through targetted
+                            marketing</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <div className="image">
+                            <img src="images/icon-4.png" alt="" className="img-fluid" />
+                        </div>
+                        <p>We expose startups to venture capitalists and angel investors based on their stage of
+                            funding</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section className="unique-section text-center">
+        <div className="custom-container">
+            <h2 className=" heading">What makes Hesteller Unique</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="item">
+                        <span>1</span>
+                        <p>Founders have an all-in-one solution where they can ideate to launch their ideas with
+                            partners that care</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <span>2</span>
+                        <p>Our team is made up of experienced technology founders from different industries and regions
+                        </p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <span>3</span>
+                        <p>Technology, Marketing & Design are all captured under one umbrella - look no further</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="item">
+                        <span>4</span>
+                        <p>Our network gives access to companies that would otherwise be hard to reach</p>
+                    </div>
+                </div>
+            </div>
+            <div className="btn-wrapper">
+                <button className="btn btn-primary">Start Ideating</button>
+            </div>
+
+        </div>
+    </section>
+    <section className="investment-section text-center">
+        <div className="custom-container">
+            <h2 className=" heading">What we look for in founders we work with</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="content">
+                        <p>Hesteller builds technology products that have the potential to scale to multi-billion dollar
+                            companies.</p>
+                        <p>We are dynamic and global focused</p>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="image">
+                        <img src="images/Investment-Criteria.png" alt="" className="img-fluid" />
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </section>
+    </>
+  );
+}
+
+export default Home;
